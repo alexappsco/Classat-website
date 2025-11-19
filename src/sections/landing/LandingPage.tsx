95ea6c73d21f75503d92b40e4861469659e44da9
Eng-AlTayeb.K <altayibkhalid132001@gmail.com>
2025-11-19 09:30:04 +0200

    feat:handel learning methods & sections header

--- src/sections/landing/LandingPage.tsx
+++ src/sections/landing/LandingPage.tsx
@@ -2,12 +2,19 @@
 import Header from 'src/layouts/dashboard/header';
 import { primary } from 'src/theme/palette';
 import HeroSection from './Hero';
+import LearningMethods from './learning-methods/LearningMethods';
+import SectionHeader from './section-header/SectionHeader';
 
 export const LandingPage = () => {
   return (
     <>
       <Header />
       <HeroSection />
+      <SectionHeader
+        title="طرق التعلم المتاحة"
+        description="اختر الطريقة التي تناسبك من بين خيارات التعلم المتنوعة"
+      />
+      <LearningMethods />
     </>
   );
 };
