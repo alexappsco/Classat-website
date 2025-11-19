3f62ff8fcc4fd6a1b7fc3a2dcc906547be105b23
Eng-AlTayeb.K <altayibkhalid132001@gmail.com>
2025-11-19 12:06:07 +0200

    feat:handle disciplines section, but still some modifications

--- src/sections/landing/LandingPage.tsx
+++ src/sections/landing/LandingPage.tsx
@@ -4,17 +4,25 @@ import { primary } from 'src/theme/palette';
 import HeroSection from './Hero';
 import LearningMethods from './learning-methods/LearningMethods';
 import SectionHeader from './section-header/SectionHeader';
+import SpecializationSection from './specializations/SpecializationSection';
 
 export const LandingPage = () => {
   return (
     <>
       <Header />
       <HeroSection />
+      {/* learning methods section */}
       <SectionHeader
         title="طرق التعلم المتاحة"
         description="اختر الطريقة التي تناسبك من بين خيارات التعلم المتنوعة"
       />
       <LearningMethods />
+      {/* disciplines section >> التخصصات */}
+      <SectionHeader
+        title="استكشف التخصصات"
+        description="اختر من بين مجموعة واسعة من المجالات التعليمية"
+      />
+      <SpecializationSection />
     </>
   );
 };
