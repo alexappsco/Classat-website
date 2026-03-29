5f5de6fc171818589a39e7b6c456db94d26a32b0
Khalid Elnaghi <khalidelnaghii@gmail.com>
2026-03-29 21:48:58 +0300

    test pipline

--- src/sections/landing/Hero.tsx
+++ src/sections/landing/Hero.tsx
@@ -57,7 +57,8 @@ export default function HeroSection() {
             color: primaryMain,
           }}
         >
-          منصتك للتعلم           <br />
+          منصتك للتعلم المباشر
+          <br />
           والتفاعل الحقيقي
         </Typography>
       </Box>
